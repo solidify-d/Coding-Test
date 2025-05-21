1fff81bfdfd43edc3375533d6190cf246404988a
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-21 10:50:14 +0900

    feat: BOJ_1152 (단어의 개수)

---
+++ Java/src/BOJ_1152.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine().trim(); // 앞뒤 공백 제거
+
+        if (input.isEmpty()) {
+            System.out.println(0); // 입력이 공백뿐일 경우
+        } else {
+            String[] words = input.split("\\s+"); // 하나 이상의 공백 기준으로 split
+            System.out.println(words.length);
+        }
+
+        br.close();
+    }
+}
