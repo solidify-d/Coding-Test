a436a14e5f7e4eb7827a697aceb8e99744cc6763
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-21 17:37:09 +0900

    feat: BOJ_10869 (사칙연산)
    문제 링크: https://www.acmicpc.net/problem/10869

---
+++ Java/src/BOJ_10869.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int a = Integer.parseInt(inputs[0]);
+        int b = Integer.parseInt(inputs[1]);
+
+        int[] results = {
+                a + b,
+                a - b,
+                a * b,
+                a / b,
+                a % b
+        };
+
+        for (int res : results) {
+            System.out.println(res);
+        }
+
+        br.close();
+    }
+}
+
+// 메모리 : 14332KB
+// 시간 : 108ms
