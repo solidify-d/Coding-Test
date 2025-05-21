7d5adf95cc69d1b4f5844e831408423c9efce943
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-21 16:26:25 +0900

    feat: BOJ_2475 (검증수)
    문제 링크: https://www.acmicpc.net/problem/2475

---
+++ Java/src/BOJ_2475.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int sqSum = 0;
+
+        for (String input : inputs) {
+            int num = Integer.parseInt(input);
+            sqSum += num * num;
+        }
+
+        System.out.println(sqSum % 10);
+    }
+}
+
+// 메모리 : 14172KB
+// 시간 : 124ms
