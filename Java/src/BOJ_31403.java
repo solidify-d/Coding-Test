2a14641c11befe445b18f92399b6c8b207337d87
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-21 13:55:32 +0900

    feat: BOJ_31403 (A+B-C)
    https://www.acmicpc.net/problem/31403

---
+++ Java/src/BOJ_31403.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_31403 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstInput = scanner.nextLine();
+        String secondInput = scanner.nextLine();
+        int thirdInput = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(Integer.parseInt(firstInput) + Integer.parseInt(secondInput) - thirdInput);
+        System.out.println((Integer.parseInt(firstInput + secondInput)) - thirdInput);
+    }
+}
