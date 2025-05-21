90f2f3eddb1d79ebe37f93d520fdc73635e0fbbb
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-21 13:54:08 +0900

    feat: BOJ_2562 (최댓값)

---
+++ Java/src/BOJ_2562.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BOJ_2562 {
+    public static void main(String[] args) {
+        final int TOTAL_NUMBERS = 9;
+        Scanner scanner = new Scanner(System.in);
+
+        int maxValue = Integer.MIN_VALUE;
+        int position = 0;
+
+        for (int i = 0; i < TOTAL_NUMBERS; i++) {
+            int input = scanner.nextInt();
+
+            if (input > maxValue) {
+                maxValue = input;
+                position = i + 1; // 1-based index
+            }
+        }
+
+        System.out.println(maxValue);
+        System.out.println(position);
+
+        scanner.close();
+    }
+}
