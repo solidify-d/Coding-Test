c56e0301b46e78f0aca805415dae5085d918a338
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-23 09:35:45 +0900

    feat: BOJ_10172 (개)
    문제 링크: https://www.acmicpc.net/problem/10172

---
+++ Java/src/BOJ_10172.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class BOJ_10172 { // 개
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");;
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+// 메모리 : 13892KB
+// 시간 : 92ms
+// 링크 : https://www.acmicpc.net/problem/10172
