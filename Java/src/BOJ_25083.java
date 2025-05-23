63803802359a8f49bea5cdbc76edaa224760ad5f
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-23 17:00:49 +0900

    feat: BOJ_25083 (새싹)
    문제 링크: https://www.acmicpc.net/problem/25083

---
+++ Java/src/BOJ_25083.java
@@ -0,0 +1,17 @@
+package Class_One;
+
+import java.io.IOException;
+
+public class BOJ_25083 { // 문제: 아스키 코드
+    public static void main(String[] args) throws IOException {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
+// 메모리 : 13896KB
+// 시간 : 96ms
+// 링크 : https://www.acmicpc.net/problem/25083
