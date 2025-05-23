e8d56436267e2b096c7907895bf31274ff03ba5c
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-23 17:02:16 +0900

    feat: BOJ_30802 (웰컴 키트)
    문제 링크: https://www.acmicpc.net/problem/30802

---
+++ Java/src/BOJ_30802.java
@@ -0,0 +1,27 @@
+package Class_Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_30802 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int participants = Integer.parseInt(br.readLine());
+        String[] sizes = br.readLine().split(" ");
+        String[] packageCount = br.readLine().split(" ");
+        br.close();
+        int numShirtPack = 0;
+        for (String size : sizes) {
+            numShirtPack += (int) Math.ceil(((double) Integer.parseInt(size)) / Integer.parseInt(packageCount[0]));
+        }
+        int numPencilPack = participants / Integer.parseInt(packageCount[1]);
+        int remainder = participants - numPencilPack * Integer.parseInt(packageCount[1]);
+        System.out.println(numShirtPack);
+        System.out.println(numPencilPack + " " + remainder);
+    }
+}
+
+
+// 메모리 : KB
+// 시간 : ms
