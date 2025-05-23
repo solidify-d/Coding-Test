ec465137c6b8a47878eb0820dbc05d415f3f31e8
Hyeseong Bak <hsbak.business@gmail.com>
2025-05-23 09:35:16 +0900

    feat: BOJ_10171 (고양이)
    문제 링크: https://www.acmicpc.net/problem/10171

---
+++ Java/src/BOJ_10171.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class BOJ_10171 { // 고양이
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+// 메모리 : 13900KB
+// 시간 : 100ms
+// 링크 : https://www.acmicpc.net/problem/10171
