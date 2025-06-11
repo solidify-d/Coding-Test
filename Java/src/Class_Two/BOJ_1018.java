1a8013405bbb074a7ac5c481002f25b0d6414798
Hyeseong Bak <hsbak.business@gmail.com>
2025-06-11 17:47:35 +0900

    temp: BOJ_11866

--- Java/src/Class_Two/BOJ_1018.java
+++ Java/src/Class_Two/BOJ_1018.java
@@ -4,56 +4,24 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_1018 {
-    public static int countDiff(StringBuilder first, StringBuilder second){
-        int count = 0;
-        for(int i = 0; i < first.length(); i++){
-            if(first.charAt(i) != second.charAt(i)){
-                count++;
-            }
-        }
-        return count;
-    }
+
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] repeats = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        String[] inputs = new String[repeats[0]];
 
-        StringBuilder startWhite = new StringBuilder();
-        StringBuilder startBlack = new StringBuilder();
-        char temp = 'W';
-        boolean first = true;
+        String startBlack = "BWBWBWBW";
+        String startWhite = "WBWBWBWB";
 
-        for(int i = 0; i < repeats[0]; i++) {
-            for (int z = 0; z < repeats[1]; z++) {
-                if (first) {
-                    startWhite.append('W');
-                    startBlack.append('B');
-                    first = false;
-                } else {
-                    if (temp == 'W') {
-                        startWhite.append('B');
-                        startBlack.append('W');
-                        temp = 'B';
-                    } else {
-                        startWhite.append('W');
-                        startBlack.append('B');
-                        temp = 'W';
-                    }
-                }
-            }
-            if (temp == 'W') {
-                temp = 'B';
-            } else {
-                temp = 'W';
-            }
-            startWhite.append('\n');
-            startBlack.append('\n');
-        }
-
-        StringBuilder input = new StringBuilder();
         for(int i = 0; i < repeats[0]; i++){
-            input.append(br.readLine()).append("\n");
+            inputs[i] = br.readLine();
         }
-        System.out.println(Math.min(countDiff(input, startBlack), countDiff(input, startWhite)));
+
+        for(int i = 0; i < repeats[0] - 7; i++){
+            for(int j = 0; j < inputs[0].length(); j++){
+                inputs[i].substring(i, i+8);
+            }
+        }
     }
 }
---
+++ Java/src/Class_Two/BOJ_11866.java
@@ -0,0 +1,24 @@
+package Class_Two;
+
+import java.nio.Buffer;
+import java.util.*;
+import java.io.*;
+
+public class BOJ_11866 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 인풋 값을 받는다.
+        // N K가 주어진다.
+        // K번째 사람을 계속해서 죽인다.
+        // N명의 사람이 1번째 자리부터 N번째 자리까지 앉아있다.
+        // Queue 를 .toString()을 써서 프린트 해야 할 것 같은 느낌이 든다.
+        // 남은 숫자가 K보다 작다면 남은 사람은 순차적으로 죽인다.
+        
+        // 1) 1부터 N까지를 담은 LinkedList를 하나 만든다.
+        // 2) next를 N만큼 하고,
+        // 3) 해당 인덱스에 들어있던 값을 큐에 담는다.
+        // 4)
+
+
+    }
+}
